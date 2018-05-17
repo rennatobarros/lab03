f20f64059cb728e64a71802a1c1e40100ea51ae6
Renato Barros <rennatobarros@gmail.com>
2018-05-17 17:37:21 -0300

    Main adicionado

---
+++ prog02-03/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+#include "conta.hpp"
+#include "movimentacao.hpp"
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+	
+	int op;
+
+	do{
+		cout << ">>>>>> Digite uma das opçoes abaixo: " << endl;
+		cout << "(1) Cadastrar conta" << endl;
+		cout << "(2) Realizar saque" << endl;
+		cout << "(3) Realizar transferência" << endl;
+		cout << "(4) Extrato da conta" << endl;
+		cout << "(5) Realizar depósito" << endl;
+		cout << "(6) Excluir conta" << endl;
+		cout << "(0) Sair" << endl;
+
+		cin >> op;
+
+		switch(op){
+			case 1:
+					break;
+			case 2:
+					break;
+			case 3:
+					break;
+			case 4:
+					break;
+			case 5:
+					break;
+			case 6:
+					break;
+			case 0:
+					return 0;
+					break;
+			default:
+					cout << "Opção inválida!!!" << endl;
+		};
+	}
+
+	return 0;
+}
